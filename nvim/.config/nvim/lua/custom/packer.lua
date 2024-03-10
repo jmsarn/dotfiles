19caabc6c59dd132b4dde1d5581731e2291f3fab
James Arnold <james@rs21.io>
2024-03-09 17:31:45 -0700

    Restructure LSP configuration

--- nvim/.config/nvim/lua/custom/packer.lua
+++ nvim/.config/nvim/lua/custom/packer.lua
@@ -1,75 +1,144 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
-    use {
-        'nvim-telescope/telescope.nvim', branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
-    use({
-        'maxmx03/solarized.nvim',
-        as = "solarized",
-        config = function()
-            vim.o.background = 'dark'
-            vim.cmd('colorscheme solarized')
-        end
-    })
-    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-    use('theprimeagen/harpoon')
-    use('mbbill/undotree')
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
-        requires = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' },
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
+return require("packer").startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
+	use({
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	use({
+		"maxmx03/solarized.nvim",
+		as = "solarized",
+		config = function()
+			vim.o.background = "dark"
+			vim.cmd("colorscheme solarized")
+		end,
+	})
+	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+	use("theprimeagen/harpoon")
+	use("mbbill/undotree")
+	use({
+		"neovim/nvim-lspconfig",
+		requires = {
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
+			{ "WhoIsSethDaniel/mason-tool-installer.nvim" },
+			{ "j-hui/fidget.nvim", opts = {} },
+		},
+	})
+	use("b0o/schemastore.nvim")
+	use({ -- Autocompletion
+		"hrsh7th/nvim-cmp",
+		--event = 'InsertEnter',
+		requires = {
+			-- Snippet Engine & its associated nvim-cmp source
+			{
+				"L3MON4D3/LuaSnip",
+				build = (function()
+					-- Build Step is needed for regex support in snippets
+					-- This step is not supported in many windows environments
+					-- Remove the below condition to re-enable on windows
+					if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+						return
+					end
+					return "make install_jsregexp"
+				end)(),
+			},
+			"saadparwaiz1/cmp_luasnip",
 
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'hrsh7th/cmp-buffer' },
-            { 'hrsh7th/cmp-path' },
-            { 'saadparwaiz1/cmp_luasnip' },
-            { 'hrsh7th/cmp-nvim-lua' },
-            { 'rafamadriz/friendly-snippets' },
-        }
-    }
-    use('m4xshen/autoclose.nvim')
-    use('b0o/schemastore.nvim')
-    use {
-        "github/copilot.vim",
-        lazy = false,
-        config = function()
-            vim.g.copilot_no_tab_map = true;
-            vim.g.copilot_assume_mapped = true;
-            vim.g.copilot_tab_fallback = "";
-        end
-    }
-    use({ -- Autoformat
-        'stevearc/conform.nvim',
-        config = function()
-            require('conform').setup({
-                notify_on_error = true,
-                format_on_save = {
-                    timeout_ms = 500,
-                    lsp_fallback = true,
-                },
-                formatters_by_ft = {
-                    lua = { 'stylua' },
-                    -- Conform can also run multiple formatters sequentially
-                    python = { "isort", "black" },
-                    --
-                    -- You can use a sub-list to tell conform to run *until* a formatter
-                    -- is found.
-                    -- javascript = { { "prettierd", "prettier" } },
-                },
-            })
-        end
-    })
+			-- Adds other completion capabilities.
+			--  nvim-cmp does not ship with all sources by default. They are split
+			--  into multiple repos for maintenance purposes.
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-path",
+
+			-- If you want to add a bunch of pre-configured snippets,
+			--    you can use this plugin to help you. It even has snippets
+			--    for various frameworks/libraries/etc. but you will have to
+			--    set up the ones that are useful for you.
+			-- 'rafamadriz/friendly-snippets',
+		},
+		config = function()
+			-- See `:help cmp`
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			luasnip.config.setup({})
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				completion = { completeopt = "menu,menuone,noinsert" },
+
+				-- For an understanding of why these mappings were
+				-- chosen, you will need to read `:help ins-completion`
+				--
+				-- No, but seriously. Please read `:help ins-completion`, it is really good!
+				mapping = cmp.mapping.preset.insert({
+					["<C-Space>"] = cmp.mapping.complete({}),
+					["<C-e>"] = cmp.mapping.close(),
+					["<C-u>"] = cmp.mapping.scroll_docs(-4),
+					["<C-d>"] = cmp.mapping.scroll_docs(4),
+					["<CR>"] = cmp.mapping.confirm({
+						behavior = cmp.ConfirmBehavior.Replace,
+						select = false,
+					}),
+
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+				}),
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+					{ name = "path" },
+				},
+			})
+		end,
+	})
+	use({
+		"github/copilot.vim",
+		lazy = false,
+		config = function()
+			vim.g.copilot_no_tab_map = true
+			vim.g.copilot_assume_mapped = true
+			vim.g.copilot_tab_fallback = ""
+		end,
+	})
+	use("m4xshen/autoclose.nvim")
+	use({ -- Autoformat
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup({
+				notify_on_error = true,
+				format_on_save = {
+					timeout_ms = 500,
+					lsp_fallback = true,
+				},
+				formatters_by_ft = {
+					lua = { "stylua" },
+					python = { "isort", "black" },
+				},
+			})
+		end,
+	})
 end)
--- nvim/.config/nvim/plugin/packer_compiled.lua
+++ nvim/.config/nvim/plugin/packer_compiled.lua
@@ -74,25 +74,20 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
+  LuaSnip = {
+    loaded = true,
+    path = "/home/james/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    url = "https://github.com/L3MON4D3/LuaSnip"
+  },
   ["autoclose.nvim"] = {
     loaded = true,
     path = "/home/james/.local/share/nvim/site/pack/packer/start/autoclose.nvim",
     url = "https://github.com/m4xshen/autoclose.nvim"
   },
-  ["cmp-buffer"] = {
-    loaded = true,
-    path = "/home/james/.local/share/nvim/site/pack/packer/start/cmp-buffer",
-    url = "https://github.com/hrsh7th/cmp-buffer"
-  },
   ["cmp-nvim-lsp"] = {
     loaded = true,
     path = "/home/james/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
-  },
-  ["cmp-nvim-lua"] = {
-    loaded = true,
-    path = "/home/james/.local/share/nvim/site/pack/packer/start/cmp-nvim-lua",
-    url = "https://github.com/hrsh7th/cmp-nvim-lua"
   },
   ["cmp-path"] = {
     loaded = true,
@@ -105,7 +100,7 @@ _G.packer_plugins = {
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
   ["conform.nvim"] = {
-    config = { "\27LJ\2\2Õ\1\0\0\4\0\f\0\0156\0\0\0'\1\1\0B\0\2\0029\0\2\0005\1\3\0005\2\4\0=\2\5\0015\2\a\0005\3\6\0=\3\b\0025\3\t\0=\3\n\2=\2\v\1B\0\2\1K\0\1\0\21formatters_by_ft\vpython\1\3\0\0\nisort\nblack\blua\1\0\0\1\2\0\0\vstylua\19format_on_save\1\0\2\17lsp_fallback\2\15timeout_ms\3ô\3\1\0\1\20notify_on_error\1\nsetup\fconform\frequire\0" },
+    config = { "\27LJ\2\2Õ\1\0\0\4\0\f\0\0156\0\0\0'\1\1\0B\0\2\0029\0\2\0005\1\3\0005\2\4\0=\2\5\0015\2\a\0005\3\6\0=\3\b\0025\3\t\0=\3\n\2=\2\v\1B\0\2\1K\0\1\0\21formatters_by_ft\vpython\1\3\0\0\nisort\nblack\blua\1\0\0\1\2\0\0\vstylua\19format_on_save\1\0\2\17lsp_fallback\2\15timeout_ms\3ô\3\1\0\1\20notify_on_error\2\nsetup\fconform\frequire\0" },
     loaded = true,
     path = "/home/james/.local/share/nvim/site/pack/packer/start/conform.nvim",
     url = "https://github.com/stevearc/conform.nvim"
@@ -116,25 +111,25 @@ _G.packer_plugins = {
     path = "/home/james/.local/share/nvim/site/pack/packer/start/copilot.vim",
     url = "https://github.com/github/copilot.vim"
   },
-  ["friendly-snippets"] = {
+  ["fidget.nvim"] = {
     loaded = true,
-    path = "/home/james/.local/share/nvim/site/pack/packer/start/friendly-snippets",
-    url = "https://github.com/rafamadriz/friendly-snippets"
+    path = "/home/james/.local/share/nvim/site/pack/packer/start/fidget.nvim",
+    url = "https://github.com/j-hui/fidget.nvim"
   },
   harpoon = {
     loaded = true,
     path = "/home/james/.local/share/nvim/site/pack/packer/start/harpoon",
     url = "https://github.com/theprimeagen/harpoon"
   },
-  ["lsp-zero.nvim"] = {
-    loaded = true,
-    path = "/home/james/.local/share/nvim/site/pack/packer/start/lsp-zero.nvim",
-    url = "https://github.com/VonHeikemen/lsp-zero.nvim"
-  },
   ["mason-lspconfig.nvim"] = {
     loaded = true,
     path = "/home/james/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
     url = "https://github.com/williamboman/mason-lspconfig.nvim"
+  },
+  ["mason-tool-installer.nvim"] = {
+    loaded = true,
+    path = "/home/james/.local/share/nvim/site/pack/packer/start/mason-tool-installer.nvim",
+    url = "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim"
   },
   ["mason.nvim"] = {
     loaded = true,
@@ -142,6 +137,7 @@ _G.packer_plugins = {
     url = "https://github.com/williamboman/mason.nvim"
   },
   ["nvim-cmp"] = {
+    config = { "\27LJ\2\2-\0\1\3\1\2\0\5-\1\0\0009\1\0\0019\2\1\0B\1\2\1K\0\1\0\1À\tbody\15lsp_expandR\0\1\2\1\2\0\f-\1\0\0009\1\0\1B\1\1\2\15\0\1\0X\2\4€-\1\0\0009\1\1\1B\1\1\1X\1\2€\18\1\0\0B\1\1\1K\0\1\0\0À\21select_next_item\fvisibleR\0\1\2\1\2\0\f-\1\0\0009\1\0\1B\1\1\2\15\0\1\0X\2\4€-\1\0\0009\1\1\1B\1\1\1X\1\2€\18\1\0\0B\1\1\1K\0\1\0\0À\21select_prev_item\fvisibleí\4\1\0\t\0'\0H6\0\0\0'\1\1\0B\0\2\0026\1\0\0'\2\2\0B\1\2\0029\2\3\0019\2\4\0024\3\0\0B\2\2\0019\2\4\0005\3\b\0005\4\6\0003\5\5\0=\5\a\4=\4\t\0035\4\n\0=\4\v\0039\4\f\0009\4\r\0049\4\14\0045\5\16\0009\6\f\0009\6\15\0064\a\0\0B\6\2\2=\6\17\0059\6\f\0009\6\18\6B\6\1\2=\6\19\0059\6\f\0009\6\20\6)\aüÿB\6\2\2=\6\21\0059\6\f\0009\6\20\6)\a\4\0B\6\2\2=\6\22\0059\6\f\0009\6\23\0065\a\26\0009\b\24\0009\b\25\b=\b\27\aB\6\2\2=\6\28\0059\6\f\0003\a\29\0005\b\30\0B\6\3\2=\6\31\0059\6\f\0003\a \0005\b!\0B\6\3\2=\6\"\5B\4\2\2=\4\f\0034\4\4\0005\5#\0>\5\1\0045\5$\0>\5\2\0045\5%\0>\5\3\4=\4&\3B\2\2\0012\0\0€K\0\1\0\fsources\1\0\1\tname\tpath\1\0\1\tname\fluasnip\1\0\1\tname\rnvim_lsp\f<S-Tab>\1\3\0\0\6i\6s\0\n<Tab>\1\3\0\0\6i\6s\0\t<CR>\rbehavior\1\0\1\vselect\1\fReplace\20ConfirmBehavior\fconfirm\n<C-d>\n<C-u>\16scroll_docs\n<C-e>\nclose\14<C-Space>\1\0\0\rcomplete\vinsert\vpreset\fmapping\15completion\1\0\1\16completeopt\26menu,menuone,noinsert\fsnippet\1\0\0\vexpand\1\0\0\0\nsetup\vconfig\fluasnip\bcmp\frequire\0" },
     loaded = true,
     path = "/home/james/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
@@ -196,8 +192,12 @@ try_loadstring("\27LJ\2\2[\0\0\2\0\6\0\t6\0\0\0009\0\1\0'\1\3\0=\1\2\0006\0\0\00
 time([[Config for solarized]], false)
 -- Config for: conform.nvim
 time([[Config for conform.nvim]], true)
-try_loadstring("\27LJ\2\2Õ\1\0\0\4\0\f\0\0156\0\0\0'\1\1\0B\0\2\0029\0\2\0005\1\3\0005\2\4\0=\2\5\0015\2\a\0005\3\6\0=\3\b\0025\3\t\0=\3\n\2=\2\v\1B\0\2\1K\0\1\0\21formatters_by_ft\vpython\1\3\0\0\nisort\nblack\blua\1\0\0\1\2\0\0\vstylua\19format_on_save\1\0\2\17lsp_fallback\2\15timeout_ms\3ô\3\1\0\1\20notify_on_error\1\nsetup\fconform\frequire\0", "config", "conform.nvim")
+try_loadstring("\27LJ\2\2Õ\1\0\0\4\0\f\0\0156\0\0\0'\1\1\0B\0\2\0029\0\2\0005\1\3\0005\2\4\0=\2\5\0015\2\a\0005\3\6\0=\3\b\0025\3\t\0=\3\n\2=\2\v\1B\0\2\1K\0\1\0\21formatters_by_ft\vpython\1\3\0\0\nisort\nblack\blua\1\0\0\1\2\0\0\vstylua\19format_on_save\1\0\2\17lsp_fallback\2\15timeout_ms\3ô\3\1\0\1\20notify_on_error\2\nsetup\fconform\frequire\0", "config", "conform.nvim")
 time([[Config for conform.nvim]], false)
+-- Config for: nvim-cmp
+time([[Config for nvim-cmp]], true)
+try_loadstring("\27LJ\2\2-\0\1\3\1\2\0\5-\1\0\0009\1\0\0019\2\1\0B\1\2\1K\0\1\0\1À\tbody\15lsp_expandR\0\1\2\1\2\0\f-\1\0\0009\1\0\1B\1\1\2\15\0\1\0X\2\4€-\1\0\0009\1\1\1B\1\1\1X\1\2€\18\1\0\0B\1\1\1K\0\1\0\0À\21select_next_item\fvisibleR\0\1\2\1\2\0\f-\1\0\0009\1\0\1B\1\1\2\15\0\1\0X\2\4€-\1\0\0009\1\1\1B\1\1\1X\1\2€\18\1\0\0B\1\1\1K\0\1\0\0À\21select_prev_item\fvisibleí\4\1\0\t\0'\0H6\0\0\0'\1\1\0B\0\2\0026\1\0\0'\2\2\0B\1\2\0029\2\3\0019\2\4\0024\3\0\0B\2\2\0019\2\4\0005\3\b\0005\4\6\0003\5\5\0=\5\a\4=\4\t\0035\4\n\0=\4\v\0039\4\f\0009\4\r\0049\4\14\0045\5\16\0009\6\f\0009\6\15\0064\a\0\0B\6\2\2=\6\17\0059\6\f\0009\6\18\6B\6\1\2=\6\19\0059\6\f\0009\6\20\6)\aüÿB\6\2\2=\6\21\0059\6\f\0009\6\20\6)\a\4\0B\6\2\2=\6\22\0059\6\f\0009\6\23\0065\a\26\0009\b\24\0009\b\25\b=\b\27\aB\6\2\2=\6\28\0059\6\f\0003\a\29\0005\b\30\0B\6\3\2=\6\31\0059\6\f\0003\a \0005\b!\0B\6\3\2=\6\"\5B\4\2\2=\4\f\0034\4\4\0005\5#\0>\5\1\0045\5$\0>\5\2\0045\5%\0>\5\3\4=\4&\3B\2\2\0012\0\0€K\0\1\0\fsources\1\0\1\tname\tpath\1\0\1\tname\fluasnip\1\0\1\tname\rnvim_lsp\f<S-Tab>\1\3\0\0\6i\6s\0\n<Tab>\1\3\0\0\6i\6s\0\t<CR>\rbehavior\1\0\1\vselect\1\fReplace\20ConfirmBehavior\fconfirm\n<C-d>\n<C-u>\16scroll_docs\n<C-e>\nclose\14<C-Space>\1\0\0\rcomplete\vinsert\vpreset\fmapping\15completion\1\0\1\16completeopt\26menu,menuone,noinsert\fsnippet\1\0\0\vexpand\1\0\0\0\nsetup\vconfig\fluasnip\bcmp\frequire\0", "config", "nvim-cmp")
+time([[Config for nvim-cmp]], false)
 -- Config for: copilot.vim
 time([[Config for copilot.vim]], true)
 try_loadstring("\27LJ\2\2€\1\0\0\2\0\6\0\r6\0\0\0009\0\1\0+\1\2\0=\1\2\0006\0\0\0009\0\1\0+\1\2\0=\1\3\0006\0\0\0009\0\1\0'\1\5\0=\1\4\0K\0\1\0\5\25copilot_tab_fallback\26copilot_assume_mapped\23copilot_no_tab_map\6g\bvim\0", "config", "copilot.vim")
