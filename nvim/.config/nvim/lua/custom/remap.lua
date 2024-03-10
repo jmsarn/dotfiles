4a1630355a967a3b4f062dfc8f790299b8f8cce8
James Arnold <james@rs21.io>
2024-03-09 17:31:45 -0700

    Restructure LSP configuration

--- nvim/.config/nvim/lua/custom/remap.lua
+++ nvim/.config/nvim/lua/custom/remap.lua
@@ -8,8 +8,8 @@ vim.keymap.set("n", "<leader>w", function()
     vim.cmd.wincmd("l")
 end)
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+--vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+--vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
