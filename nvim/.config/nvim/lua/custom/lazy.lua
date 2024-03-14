975a4238af68d6ad40cd7d4628a8732766f71213
James Arnold <james@rs21.io>
2024-03-14 17:19:34 -0600

    Remove reference highlighting

--- nvim/.config/nvim/lua/custom/lazy.lua
+++ nvim/.config/nvim/lua/custom/lazy.lua
@@ -193,24 +193,6 @@ require("lazy").setup({
                     -- WARN: This is not Goto Definition, this is Goto Declaration.
                     --  For example, in C this would take you to the header
                     map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-
-                    -- The following two autocommands are used to highlight references of the
-                    -- word under your cursor when your cursor rests there for a little while.
-                    --    See `:help CursorHold` for information about when this is executed
-                    --
-                    -- When you move your cursor, the highlights will be cleared (the second autocommand).
-                    local client = vim.lsp.get_client_by_id(event.data.client_id)
-                    if client and client.server_capabilities.documentHighlightProvider then
-                        vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-                            buffer = event.buf,
-                            callback = vim.lsp.buf.document_highlight,
-                        })
-
-                        vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-                            buffer = event.buf,
-                            callback = vim.lsp.buf.clear_references,
-                        })
-                    end
                 end,
             })
 
@@ -231,19 +213,6 @@ require("lazy").setup({
             --  - settings (table): Override the default settings passed when initializing the server.
             --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
             local servers = {
-                -- clangd = {},
-                -- gopls = {},
-                -- pyright = {},
-                -- rust_analyzer = {},
-                -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-                --
-                -- Some languages (like typescript) have entire language plugins that can be useful:
-                --    https://github.com/pmizio/typescript-tools.nvim
-                --
-                -- But for many setups, the LSP (`tsserver`) will work just fine
-                -- tsserver = {},
-                --
-
                 lua_ls = {
                     -- cmd = {...},
                     -- filetypes { ...},
@@ -273,6 +242,7 @@ require("lazy").setup({
                 dockerls = {},
                 gopls = {},
                 pyright = {},
+                tsserver = {},
                 jsonls = {
                     settings = {
                         json = {
