e009f93565f626fbd9611ea38f284f0585c776e2
James Arnold <james@rs21.io>
2024-03-21 09:47:35 -0600

    Add obsidian plugin

--- nvim/.config/nvim/lua/custom/lazy.lua
+++ nvim/.config/nvim/lua/custom/lazy.lua
@@ -530,5 +530,43 @@ require("lazy").setup({
             dap.setup(vim.g.python3_host_prog)
             dap.test_runner = "pytest"
         end
-    }
+    },
+    {
+        "epwalsh/obsidian.nvim",
+        version = "*",
+        lazy = true,
+        ft = "markdown",
+        depedndencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        opts = {
+            workspaces = {
+                {
+                    name = "personal",
+                    path = "~/github.com/jmsarn/obsidian-vaults/personal",
+                },
+                {
+                    name = "work",
+                    path = "~/github.com/jmsarn/obsidian-vaults/work",
+                },
+            },
+            ui = {
+                enable = true,         -- set to false to disable all additional syntax features
+                update_debounce = 200, -- update delay after a text change (in milliseconds)
+                -- Define how various check-boxes are displayed
+                checkboxes = {
+                    -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
+                    [" "] = { char = "󰄱", hl_group = "ObsidianTodo" },
+                    ["x"] = { char = "", hl_group = "ObsidianDone" },
+                    [">"] = { char = "", hl_group = "ObsidianRightArrow" },
+                    ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+                    -- Replace the above with this if you don't have a patched font:
+                    -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
+                    -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
+
+                    -- You can also add more custom ones...
+                },
+            },
+        },
+    },
 })
