e5f287eeec89ec520339d39253eae4dcc0f30fb6
James Arnold <james@rs21.io>
2024-03-21 09:45:42 -0600

    Add linting for text files

--- nvim/.config/nvim/lua/custom/lazy.lua
+++ nvim/.config/nvim/lua/custom/lazy.lua
@@ -326,6 +326,27 @@ require("lazy").setup({
         end,
     },
     {
+        "mfussenegger/nvim-lint",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            local lint = require("lint")
+            lint.linters_by_ft = {
+                markdown = { "vale" },
+                -- python = { "pylint", "ruff" },
+                rst = { "vale" },
+                text = { "vale" },
+            }
+
+            local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+            vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+                group = lint_augroup,
+                callback = function()
+                    require("lint").try_lint()
+                end,
+            })
+        end,
+    },
+    {
         "stevearc/conform.nvim",
         opts = {
             notify_on_error = false,
