242911ee4c70f86d0fc82aa4cc3dd01a323222c5
James Arnold <james@rs21.io>
2024-03-25 16:43:32 -0600

    Update harpoon configuration

--- nvim/.config/nvim/lua/custom/lazy.lua
+++ nvim/.config/nvim/lua/custom/lazy.lua
@@ -101,23 +101,40 @@ require("lazy").setup({
 
             harpoon:setup()
 
+            local conf = require("telescope.config").values
+            local function toggle_telescope(harpoon_files)
+                local file_paths = {}
+                for _, item in ipairs(harpoon_files.items) do
+                    table.insert(file_paths, item.value)
+                end
+
+                require("telescope.pickers").new({}, {
+                    prompt_title = "Harpoon",
+                    finder = require("telescope.finders").new_table({
+                        results = file_paths,
+                    }),
+                    previewer = conf.file_previewer({}),
+                    sorter = conf.generic_sorter({}),
+                }):find()
+            end
+
             vim.keymap.set("n", "<leader>a", function()
                 harpoon:list():append()
             end)
             vim.keymap.set("n", "<C-e>", function()
-                harpoon.ui:toggle_quick_menu(harpoon:list())
+                toggle_telescope(harpoon:list())
             end)
 
             vim.keymap.set("n", "<C-h>", function()
                 harpoon:list():select(1)
             end)
-            vim.keymap.set("n", "<C-t>", function()
+            vim.keymap.set("n", "<C-j>", function()
                 harpoon:list():select(2)
             end)
-            vim.keymap.set("n", "<C-n>", function()
+            vim.keymap.set("n", "<C-k>", function()
                 harpoon:list():select(3)
             end)
-            vim.keymap.set("n", "<C-s>", function()
+            vim.keymap.set("n", "<C-l>", function()
                 harpoon:list():select(4)
             end)
 
