b0e084edff1b118182d3fc297b310db7b8e9212b
James Arnold <james@rs21.io>
2024-04-11 19:32:53 -0600

    Update debugger config

--- nvim/.config/nvim/lua/custom/lazy.lua
+++ nvim/.config/nvim/lua/custom/lazy.lua
@@ -256,6 +256,7 @@ require("lazy").setup({
                         },
                     },
                 },
+                --bashls = {},
                 dockerls = {},
                 gopls = {},
                 ruff_lsp = {
@@ -515,7 +516,7 @@ require("lazy").setup({
         config = function()
             require("mini.ai").setup { n_lines = 500 }
             require("mini.indentscope").setup()
-            require("mini.surround").setup()
+            -- require("mini.surround").setup()
 
             local statusline = require("mini.statusline")
             statusline.setup { use_icons = vim.g.have_nerd_font }
@@ -525,23 +526,83 @@ require("lazy").setup({
             --  Check out: https://github.com/echasnovski/mini.nvim
         end
     },
-    "mfussenegger/nvim-dap",
     {
-        "rcarriga/nvim-dap-ui",
+        "mfussenegger/nvim-dap",
         dependencies = {
-            "nvim-neotest/nvim-nio"
+            "rcarriga/nvim-dap-ui",
+            "nvim-neotest/nvim-nio",
+            "williamboman/mason.nvim",
+            "jay-babu/mason-nvim-dap.nvim",
+
+            "leoluz/nvim-dap-go",
+            "mfussenegger/nvim-dap-python",
         },
         config = function()
-            require("dapui").setup()
-        end
-    },
-    {
-        "mfussenegger/nvim-dap-python",
-        config = function()
-            local dap = require("dap-python")
-            dap.setup(vim.g.python3_host_prog)
-            dap.test_runner = "pytest"
-        end
+            local dap = require("dap")
+            local dapui = require("dapui")
+
+            require("mason-nvim-dap").setup({
+                automatic_setup = true,
+                handlers = {},
+                ensure_installed = {
+                    "debugpy"
+                }
+            })
+
+            vim.keymap.set('n', '<F5>',
+                function()
+                    if vim.fn.filereadable(".vscode/launch.json") then
+                        require("dap.ext.vscode").load_launchjs(nil, { debugpy = { "python" } })
+                    end
+                    dap.continue()
+                end,
+                { desc = 'Debug: Start/Continue' })
+            vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
+            vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
+            vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
+            vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+            vim.keymap.set('n', '<leader>B', function()
+                dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+            end, { desc = 'Debug: Set Breakpoint' })
+            vim.keymap.set("n", "<leader>lp",
+                ":lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>",
+                { desc = "Log breakpoint" })
+            vim.keymap.set("n", "<leader>dr", ":lua require'dap'.repl.open()<CR>", { desc = "Open REPL" })
+
+            -- Dap UI setup
+            -- For more information, see |:help nvim-dap-ui|
+            dapui.setup {
+                -- Set icons to characters that are more likely to work in every terminal.
+                --    Feel free to remove or use ones that you like more! :)
+                --    Don't feel like these are good choices.
+                icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+                controls = {
+                    icons = {
+                        pause = '⏸',
+                        play = '▶',
+                        step_into = '⏎',
+                        step_over = '⏭',
+                        step_out = '⏮',
+                        step_back = 'b',
+                        run_last = '▶▶',
+                        terminate = '⏹',
+                        disconnect = '⏏',
+                    },
+                },
+            }
+
+            -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
+            vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
+
+            dap.listeners.after.event_initialized['dapui_config'] = dapui.open
+            dap.listeners.before.event_terminated['dapui_config'] = dapui.close
+            dap.listeners.before.event_exited['dapui_config'] = dapui.close
+
+            require("dap-go").setup()
+
+            require("dap-python").setup(vim.g.python3_host_prog)
+            require("dap-python").test_runner = "pytest"
+        end,
     },
     {
         "epwalsh/obsidian.nvim",
