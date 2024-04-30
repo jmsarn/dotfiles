a5ac5deca539a4dcc01299571f26a2d8773ab601
James Arnold <james@rs21.io>
2024-04-30 09:07:48 -0600

    Rough out script for installing core software

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+GO_VERSION="1.22.2"
+
+# Install Golang
+curl -O "https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz"
+tar -C /usr/local -xzf "go${GO_VERSION}.linux-amd64.tar.gz"
+
+# Install Rust
+curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+
+# Install neovim
+add-apt-repository ppa:neovim-ppa/unstable
+apt-get update
+apt-get install -y neovim
+
+# Install bat
+apt-get install -y bat 
+
+# Install eza
+cargo install eza
