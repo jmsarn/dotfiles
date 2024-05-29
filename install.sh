188badddd6e07cd1363436f3303c2557b6fe2a7b
James Arnold <james.arnold336@gmail.com>
2024-05-29 13:51:08 -0600

    Update install script

--- install.sh
+++ install.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
-GO_VERSION="1.22.2"
+GO_VERSION="1.22.3"
 
 # Install Golang
-curl -O "https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz"
-tar -C /usr/local -xzf "go${GO_VERSION}.linux-amd64.tar.gz"
+curl -OL "https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz"
+sudo tar -C /usr/local -xzf "go${GO_VERSION}.linux-amd64.tar.gz"
 
 # Install Rust
 curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
 
 # Install neovim
-add-apt-repository ppa:neovim-ppa/unstable
-apt-get update
-apt-get install -y neovim
+sudo add-apt-repository -y ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install -y neovim
+
+# Install Python3.11
+sudo add-apt-repository -y ppa:deadsnakes/ppa
+sudo apt-get update
+sudo apt-get install -y python3.11 python3.11-dev python3.11-venv
+
+# Install node version manager and node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+nvm install node
 
 # Install bat
 apt-get install -y bat 
 
 # Install eza
-cargo install eza
+~/.cargo/bin/cargo install eza
 
 # Install fzf
-apt-get install -y fzf
+sudo apt-get install -y fzf
