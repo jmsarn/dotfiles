d7cf282e2be2e4e6419c3059ec41fad5051feb49
James Arnold <james@rs21.io>
2024-12-11 15:48:18 -0700

    Reorganize nvim configuration

---
+++ nvim/.config/nvim/lua/custom/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+        ensure_installed = { "bash", "diff", "lua", "markdown", "python" },
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+    },
+    config = function(_, opts)
+        require("nvim-treesitter.configs").setup(opts)
+    end,
+}
--- nvim/.config/nvim/lua/remap.lua
+++ nvim/.config/nvim/lua/remap.lua
@@ -33,19 +33,3 @@ vim.keymap.set('n', '[d', vim.diagnostic.get_prev, { desc = 'Go to previous [D]i
 vim.keymap.set('n', ']d', vim.diagnostic.get_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-
--- Telescope buffers
-vim.keymap.set("n", "<S-b>", function()
-    require("telescope.builtin").buffers(require("telescope.themes").get_ivy({
-        sort_mru = true,
-        sort_lastused = true,
-        initial_mode = "normal",
-        -- Pre-select the current buffer
-        -- ignore_current_buffer = false,
-        -- select_current = true,
-        layout_config = {
-            -- Set preview width, 0.7 sets it to 70% of the window width
-            preview_width = 0.45,
-        },
-    }))
-end, { desc = "[P]Open telescope buffers" })
