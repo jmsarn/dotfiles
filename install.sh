68fe27841a6b95dcd693b7df4b5c30247bc019e7
James Arnold <oioi05@msn.com>
2024-12-11 14:31:08 -0700

    Update package versions

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-GO_VERSION="1.22.3"
+GO_VERSION="1.23.4"
 
-# Install tmux and stow
+# Install additional apt packages
 sudo apt-get update
-sudo apt-get install -y tmux stow
+sudo apt-get install -y bat fzf tmux stow xclip
 
 # Install Golang
 curl -OL "https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz"
@@ -12,6 +12,9 @@ sudo tar -C /usr/local -xzf "go${GO_VERSION}.linux-amd64.tar.gz"
 
 # Install Rust
 curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+
+# Install cargo packages
+~/.cargo/bin/cargo install eza
 
 # Install neovim
 sudo add-apt-repository -y ppa:neovim-ppa/unstable
@@ -28,17 +31,9 @@ mkdir -p ~/.local/venv
 python3.11 -m venv ~/.local/venv/nvim
 
 # Install node version manager and node
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install --lts
 
-# Install bat
-sudo apt-get install -y bat 
-
-# Install eza
-~/.cargo/bin/cargo install eza
-
-# Install fzf
-sudo apt-get install -y fzf
