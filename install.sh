560234b4e8820b9ff9b0a5abfc3f9c0eab76e93e
James Arnold <james@rs21.io>
2024-12-18 21:00:07 -0700

    Add direnv, yq, and uv

--- install.sh
+++ install.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+DIRENV_VERSION="2.35.0"
 FZF_VERSION="0.57.0"
 GO_VERSION="1.23.4"
+YQ_VERSION="4.44.6"
 
 # Install additional apt packages
 sudo apt-get update
 sudo apt-get install -y bat tmux stow xclip
+
+# Install direnv
+curl -LO "https://github.com/direnv/direnv/releases/download/v${DIRENV_VERSION}/direnv.linux-amd64"
+chmod +x direnv.linux-amd64
+sudo mv direnv.linux-amd64 /usr/local/bin/direnv
 
 # Install fzf
 curl -LO "https://github.com/junegunn/fzf/releases/download/v${FZF_VERSION}/fzf-${FZF_VERSION}-linux_amd64.tar.gz"
@@ -18,8 +25,14 @@ sudo tar -C /usr/local -xzf "go${GO_VERSION}.linux-amd64.tar.gz"
 # Install Rust
 curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
 
+# Install yq
+curl -LO "https://github.com/mikefarah/yq/releases/download/v${YQ_VERSION}/yq_linux_amd64"
+chmod +x yq_linux_amd64
+sudo mv yq_linux_amd64 /usr/local/bin/yq
+
 # Install cargo packages
-~/.cargo/bin/cargo install eza
+~/.cargo/bin/cargo install eza, ripgrep
+cargo install --git https://github.com/astral-sh/uv uv
 
 # Install neovim
 sudo add-apt-repository -y ppa:neovim-ppa/unstable
@@ -42,3 +55,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install --lts
 
+# Create repository directory
+mkdir -p ~/github.com
+
+# Git configuration
+git config --global core.excludesfile ~/.gitignore-global
