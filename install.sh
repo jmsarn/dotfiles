7023d39f95d2065bf41ef6000b51a789319a1d7c
James Arnold <oioi05@msn.com>
2024-12-22 12:28:48 -0700

    1password installation

--- install.sh
+++ install.sh
@@ -46,8 +46,27 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install --lts
 
+# Install 1password
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+  sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg && \
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" | \
+  sudo tee /etc/apt/sources.list.d/1password.list && \
+  sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/ && \
+  curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
+  sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol && \
+  sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22 && \
+  curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+  sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg && \
+  sudo apt update && sudo apt install -y 1password-cli
+
 # Create repository directory
 mkdir -p ~/github.com
+
+# 1password integration with direnv
+git clone https://github.com/venkytv/direnv-op.git ~/github.com/venkytv/direnv-op/
+cd ~/github.com/venkytv/direnv-op
+make install
+cd -
 
 # Git configuration
 git config --global core.excludesfile ~/.gitignore-global
