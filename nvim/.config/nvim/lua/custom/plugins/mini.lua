8b91107aded8a1a17e1891857618d67d2a58af54
James Arnold <oioi05@msn.com>
2024-12-22 12:46:15 -0700

    Enable mini.surround

--- nvim/.config/nvim/lua/custom/plugins/mini.lua
+++ nvim/.config/nvim/lua/custom/plugins/mini.lua
@@ -3,7 +3,7 @@ return {
     config = function()
         require("mini.ai").setup { n_lines = 500 }
         require("mini.indentscope").setup()
-        -- require("mini.surround").setup()
+        require("mini.surround").setup()
 
         local statusline = require("mini.statusline")
         statusline.setup { use_icons = vim.g.have_nerd_font }
